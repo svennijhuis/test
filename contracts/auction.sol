33a7768595382a84fe51aeeb025ee554fecd7135
svennijhuis <sanijhuis@live.nl>
2023-05-10 14:03:12 +0200

    test

--- contracts/auction.sol
+++ contracts/auction.sol
@@ -9,7 +9,7 @@ contract DosAuction {
   function bid() payable {
     require(msg.value > currentBid);
 
-    //If the refund fails, the entire transaction reverts.
+
     //Therefore a frontrunner who always fails will win
     if (currentFrontrunner != 0) {
       //E.g. if recipients fallback function is just revert()
