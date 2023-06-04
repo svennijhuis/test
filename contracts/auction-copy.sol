eb9f45b9eb3fc8f41d91320aea9afcb265cfe3af
svennijhuis <sanijhuis@live.nl>
2023-06-04 21:12:03 +0200

    test

--- contracts/auction-copy.sol
+++ contracts/auction-copy.sol
@@ -1,48 +1,36 @@
-pragma solidity ^0.4.15;
-
-contract DosAuction {
-    address currentFrontrunner;
-    uint currentBid;
-
-    function bid() payable {
-        require(msg.value > currentBid);
-
-        //Therefore a frontrunner who always fails will win
-        if (currentFrontrunner != 0) {
-            //E.g. if recipients fallback function is just revert()
-            require(currentFrontrunner.send(currentBid));
-        }
-
-        currentFrontrunner = msg.sender;
-        currentBid = msg.value;
-    }
-}
+pragma solidity ^0.8.0;
 
 contract SecureAuction {
-    address currentFrontrunner;
-    uint currentBid;
-    //Store refunds in mapping to avoid DoS
-    mapping(address => uint) refunds;
+    address payable public currentFrontrunner;
+    uint public currentBid;
+    mapping(address => uint) public refunds;
 
-    //Avoids "pushing" balance to users favoring "pull" architecture
+    event BidPlaced(address bidder, uint bidAmount);
+    event RefundClaimed(address bidder, uint refundAmount);
+
     function bid() external payable {
-        require(msg.value > currentBid);
+        require(
+            msg.value > currentBid,
+            "Bid amount must be higher than current bid"
+        );
 
-        if (currentFrontrunner != 0) {
+        if (currentFrontrunner != address(0)) {
             refunds[currentFrontrunner] += currentBid;
         }
 
-        currentFrontrunner = msg.sender;
+        currentFrontrunner = payable(msg.sender);
         currentBid = msg.value;
+
+        emit BidPlaced(msg.sender, msg.value);
     }
 
-    //Allows users to get their refund from auction
     function withdraw() external {
-        //Do all state manipulation before external call to
-        //avoid reentrancy attack
         uint refund = refunds[msg.sender];
-        refunds[msg.sender] = 0;
+        require(refund > 0, "No refund available");
 
-        msg.sender.send(refund);
+        refunds[msg.sender] = 0;
+        payable(msg.sender).transfer(refund);
+
+        emit RefundClaimed(msg.sender, refund);
     }
 }
--- contracts/auction.sol
+++ contracts/auction.sol
@@ -1,53 +1,36 @@
-pragma solidity ^0.4.15;
+pragma solidity ^0.8.0;
 
-//Auction susceptible to DoS attack
-contract DosAuction {
-  address currentFrontrunner;
-  uint currentBid;
-
-  //Takes in bid, refunding the frontrunner if they are outbid
-  function bid() payable {
-    require(msg.value > currentBid);
-
-
-
-    if (currentFrontrunner != 0) {
-      //E.g. if recipients fallback function is just revert()
-      require(currentFrontrunner.send(currentBid));
-    }
-
-    currentFrontrunner = msg.sender;
-    currentBid         = msg.value;
-  }
-}
-
-
-//Secure auction that cannot be DoS'd
 contract SecureAuction {
-  address currentFrontrunner;
-  uint    currentBid;
-  //Store refunds in mapping to avoid DoS
-  mapping(address => uint) refunds;
+    address payable public currentFrontrunner;
+    uint public currentBid;
+    mapping(address => uint) public refunds;
 
-  //Avoids "pushing" balance to users favoring "pull" architecture
-  function bid() payable external {
-    require(msg.value > currentBid);
+    event BidPlaced(address bidder, uint bidAmount);
+    event RefundClaimed(address bidder, uint refundAmount);
 
-    if (currentFrontrunner != 0) {
-      refunds[currentFrontrunner] += currentBid;
+    function bid() external payable {
+        require(
+            msg.value > currentBid,
+            "Bid amount must be higher than current bid"
+        );
+
+        if (currentFrontrunner != address(0)) {
+            refunds[currentFrontrunner] += currentBid;
+        }
+
+        currentFrontrunner = payable(msg.sender);
+        currentBid = msg.value;
+
+        emit BidPlaced(msg.sender, msg.value);
     }
 
-    currentFrontrunner = msg.sender;
-    currentBid         = msg.value;
-  }
+    function withdraw() external {
+        uint refund = refunds[msg.sender];
+        require(refund > 0, "No refund available");
 
-  //Allows users to get their refund from auction
-  function withdraw() external {
-    //Do all state manipulation before external call to
-    //avoid reentrancy attack
-    uint refund = refunds[msg.sender];
-    refunds[msg.sender] = 0;
+        refunds[msg.sender] = 0;
+        payable(msg.sender).transfer(refund);
 
-    msg.sender.send(refund);
-  }
+        emit RefundClaimed(msg.sender, refund);
+    }
 }
