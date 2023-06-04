efab28aa09eaa10b42b30b9d78dd6fa70cab529a
svennijhuis <sanijhuis@live.nl>
2023-06-04 22:06:39 +0200

    test

--- contracts/auction.sol
+++ contracts/auction.sol
@@ -11,7 +11,7 @@ contract SecureAuction {
     function bid() external payable {
         require(
             msg.value > currentBid,
-            "Bid amount must be higher thacurrent bid"
+            "Bid amount must be higher than current bid"
         );
 
         if (currentFrontrunner != address(0)) {
